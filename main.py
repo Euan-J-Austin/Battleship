f0c2665e1751631c4fb385cc251eef50aa572172
Euan Austin <elvismonarchy@proton.me>
2023-03-31 07:06:53 +0100

    Output grid function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 ## next step is to make code more elegant then classful
+# victory condition function
+# sunk ship function
 
 ocean_grid = [[x,y] for x in ['1','2','3','4','5','6','7','8','9','10'] for y in ['1','2','3','4','5','6','7','8','9','10']]
 target_grid = [[x,y] for x in ['1','2','3','4','5','6','7','8','9','10'] for y in ['1','2','3','4','5','6','7','8','9','10']]
@@ -48,11 +50,64 @@ def shoot(x_coord, y_coord):
     if len(x) == 3:
       if x[0] == x_coord and x[1] == y_coord:
         print("Hit!")
-      for x in target_grid:
+      for x in ocean_grid:
         if x[0] == x_coord and x[1] == y_coord:
           return x.append("Hit.")
       else:
         return print("Miss!")
+        #app miss
 
 shoot('1','1')
 print(target_grid)
+      
+def output_grid(grid):
+  temp_grid = grid.copy()
+  #use map on ocean grid to create iterator that yeilds all values for a row,
+  #transformed to bluesquare if empty, capital letter for ship name, 
+  #fire if hit
+  #though map is an iterator, you can print all values as a list()
+
+  #the first thing to do will be to transform the grid to read Blue etc.
+  # we can print ten lines at a time and impose grid coords 
+
+  name_key = {
+    'Carrier': 'C',
+    'Battleship': 'B',
+    'Cruiser': 'Cr',
+    'Submarine': 'S',
+    'Destroyer': 'D'
+  }
+  
+  x_size = (list(map(len, [x for x in grid]))) 
+
+  for x in temp_grid:
+    if len(x) == 2:
+      x.clear()
+      x.append('Blue')
+    elif len(x) == 3:
+      if x[2] == 'Carrier':
+        x.clear()
+        x.append('C')
+      elif x[2] == 'Battleship':
+        x.clear()
+        x.append('B')
+      elif x[2] == 'Cruiser':
+        x.clear()
+        x.append('Cr')
+      elif x[2] == 'Submarine':
+        x.clear()
+        x.append('S')
+      elif x[2] == 'Destroyer':
+        x.clear()
+        x.append('D')
+    elif len(x) == 4:
+      if x[3] == 'Hit.':
+        x.clear()
+        x.append('F')
+    
+  
+  return print(temp_grid)
+
+  
+
+output_grid(ocean_grid)
