6444f5915211bf3b4583a0d85d90c9221c3967b9
Euan Austin <elvismonarchy@proton.me>
2023-04-05 21:35:31 +0100

    Printing current placement grid, output grid deep copy, counting inputs so correct number of prompts, fixed placment function so checks for correct numbers of r,c

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os 
+import time 
+import copy
 
+input_counter = 0 
 ocean_grid = [[x,y] for x in ['1','2','3','4','5','6','7','8','9','10'] for y in ['1','2','3','4','5','6','7','8','9','10']]
 target_grid = [[x,y] for x in ['1','2','3','4','5','6','7','8','9','10'] for y in ['1','2','3','4','5','6','7','8','9','10']]
 computer_ocean_grid = [[x,y] for x in ['1','2','3','4','5','6','7','8','9','10'] for y in ['1','2','3','4','5','6','7','8','9','10']]
@@ -14,9 +17,11 @@ ship_size = {
 }
 
 class Placement:
-  def __int__(self):
-    pass
+  def __init__(self):
+    self.v_entry_counter = 0
+    self.h_entry_counter = 0
   def place_my_ship(self, type, alignment, row_coord, column_coord):
+    global input_counter
     h_upper_limit = int(column_coord) + int(ship_size[type])
     v_upper_limit = int(row_coord) + int(ship_size[type])
     if alignment == 'H':
@@ -25,21 +30,29 @@ class Placement:
       else:
         for x in ocean_grid:
           if x[0] == row_coord and int(column_coord) <= int(x[1]):
-            if len(x) == 3:
-              return print("Failed, contains ship.") 
-            elif int(x[1]) < h_upper_limit:
+            if self.h_entry_counter < ship_size[type]:
+              if int(x[1]) < h_upper_limit:
                 x.append(type)
+                self.h_entry_counter += 1
+              elif len(x) == 3:
+                return print("Failed, contains ship.")
+                  
     elif alignment == 'V':
       if int(row_coord) + ship_size[type] > 10:
         return print("Failed placement, outside of board.")
       for x in ocean_grid:
         if x[1] == column_coord and int(row_coord) <= int(x[0]):
-          if len(x) == 3:
-            return print("Failed, contains ship.")
-          elif int(x[0]) < v_upper_limit:
-            x.append(type)
+          if self.v_entry_counter < ship_size[type]:
+            if int(x[0]) < v_upper_limit:
+              x.append(type)
+              self.v_entry_counter += 1
+            elif len(x) == 3:
+              return print("Failed, contains ship.")
+              
     print(f"{type} placed at [{row_coord},{column_coord}].")
-    return output.output_grid()
+    input_counter += 1
+    print(input_counter)
+    return output.output_grid(ocean_grid)
 
 class PlayerActions:
   def shoot(x_coord, y_coord):
@@ -57,7 +70,7 @@ class Output:
   def __init__(self):
     pass
   def output_grid(self, grid):
-    temp_grid = grid.copy()
+    temp_grid = copy.deepcopy(ocean_grid)
     for x in temp_grid:
       if len(x) == 2:
         x.clear()
@@ -65,13 +78,13 @@ class Output:
       elif len(x) == 3:
         if x[2] == 'Carrier':
           x.clear()
-          x.append('C')
+          x.append('A')
         elif x[2] == 'Battleship':
           x.clear()
           x.append('B')
         elif x[2] == 'Cruiser':
           x.clear()
-          x.append('Cr')
+          x.append('C')
         elif x[2] == 'Submarine':
           x.clear()
           x.append('S')
@@ -83,9 +96,9 @@ class Output:
           x.clear()
           x.append('F')
 
-      no_list = ' '.join([''.join(x) for x in grid])
+    no_list = ' '.join([''.join(x) for x in temp_grid])
 
-      print(f"""
+    print(f"""
          1 2 3 4 5 6 7 8 9 10
       1  {no_list[0:19]}
       2  {no_list[20:39]}
@@ -98,15 +111,25 @@ class Output:
       9  {no_list[160:179]}
       10 {no_list[180:199]}
       """)
-  
-    return print(temp_grid)
+    return enter.placement_info()
 
 class enter_data:
+  def __init__(self):
+    pass
   def placement_info(self):
-    type = input("Enter type: ")
-    alignment = input("Enter alignment (H or V): ")
-    coords = input("Enter row and column (e.g. 7,3): ")
-    place.place_my_ship(type, alignment, coords[0], coords[2:])
+    global input_counter
+    names = ["Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer"]
+    print(input_counter)
+    if input_counter < 5:
+      print(f"Enter information for {names[input_counter]} . . .")
+      alignment = input("Enter alignment (H or V): ")
+      coords = input("Enter coords (e.g. 3,7): ")
+      print(coords.split(',')[0])
+      print(coords.split(',')[1])
+      place.place_my_ship(names[input_counter], alignment, coords.split(',')[0], coords.split(',')[1])
+    
+    
+    
     #NEXT STEP IS PRINTING A GRID AFTER THIS 
     #CONDITION FOR FINISHING PLACEMENT? 
     
